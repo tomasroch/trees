2efc555dc46be5c2bf7cbe5b6c209cbf93926673
Tomáš Roch <rochtomass@gmail.com>
2024-06-17 20:12:58 +0200

    Add minimal code and create binary tree - types of exercise

---
+++ src/components/pages/templates/tree-lib/CommonNode.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {Handle, Position} from 'reactflow';
+import {Input} from "@mui/joy";
+import {CheckCircle, Visibility} from "@mui/icons-material";
+
+export default function CommonNode({ data, id })  {
+    const [inputData, setInputData] = useState()
+    const handleIconChange = event => {
+        setInputData(data.result)
+        setColor("success")
+        setIcon(<CheckCircle style={{ color: 'green' }} />)
+    }
+
+
+    const [icon, setIcon] = useState(<Visibility onClick={handleIconChange} color="outlined" />)
+    const [color, setColor] = useState("primary")
+
+
+    const handleOnChange = event => {
+        let inputData = event.target.value;
+        setInputData(inputData)
+        inputData = (inputData.trim().split(/ +/).join(''))
+
+        if (data.result === inputData){
+            setColor("success")
+            setIcon(<CheckCircle style={{ color: 'green' }} />)
+        }
+        else if (inputData === ""){
+            setColor("primary")
+            setIcon(<Visibility onClick={handleIconChange} color="outlined" />)
+        }
+        else{
+            setColor("danger")
+            setIcon(<Visibility onClick={handleIconChange} color="outlined" />)
+        }
+
+    }
+
+    return (
+        <>
+            <Input inputProps={{ style: { textAlign: 'center'}}} sx={{width: data.width}}
+                   size="sm" placeholder={data.placeHolder} color={color} onChange={handleOnChange} value={inputData}
+                   endDecorator={icon}
+            />
+            <Handle type="target" position={Position.Top}/>
+            <Handle type="source" position={Position.Bottom}/>
+
+            {/*<Typography>{id}</Typography>*/}
+        </>
+    );
+}
---
+++ src/data/CodeTreeExample.js
@@ -0,0 +1,162 @@
+export const exampleNodes = [
+    {
+        id: '1',
+        position: {x: 0, y: 0},
+        data: {result: '0000101101101000101111', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '2',
+        position: {x: -100, y: 100},
+        data: {result: '0001011011', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '3',
+        position: {x: 0, y: 100},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '4',
+        position: {x: 100, y: 100},
+        data: {result: '00010111', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '5',
+        position: {x: -150, y: 200},
+        data: {result: '001011', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '6',
+        position: {x: -50, y: 200},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '7',
+        position: {x: 100, y: 200},
+        data: {result: '001011', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '8',
+        position: {x: -200, y: 300},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '9',
+        position: {x: -100, y: 300},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '10',
+        position: {x: 50, y: 300},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+    {
+        id: '11',
+        position: {x: 150, y: 300},
+        data: {result: '01', width:100, placeHolder: "Kód..."},
+        hidden: false,
+        type:'minimalCodeNode'
+    },
+]
+
+export const exampleEdges = [
+    {
+        id: '1-2',
+        source: '1',
+        target: '2',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '1-3',
+        source: '1',
+        target: '3',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '1-4',
+        source: '1',
+        target: '4',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '2-5',
+        source: '2',
+        target: '5',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '2-6',
+        source: '2',
+        target: '6',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '4-7',
+        source: '4',
+        target: '7',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '5-8',
+        source: '5',
+        target: '8',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '5-9',
+        source: '5',
+        target: '9',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '7-10',
+        source: '7',
+        target: '10',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+    {
+        id: '7-11',
+        source: '7',
+        target: '11',
+        hidden: false,
+        animated: true,
+        type: ''
+    },
+]
